aede5c29c895e509c426fb069bf3e801a3688190
Sebastian Sejud <sejud@statica.pl>
2015-01-21 09:35:22 +0100

    Converted iOs version to Unified API. Samples not converted!

--- AdBuddiz.Xamarin/AdBuddizHandler.iOs.cs
+++ AdBuddiz.Xamarin/AdBuddizHandler.iOs.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using MonoTouch.Foundation;
+using Foundation;
 using AdBuddiz.Xamarin;
 
 namespace AdBuddiz.Xamarin
--- AdBuddiz.Xamarin/ApiDefinition.cs
+++ AdBuddiz.Xamarin/ApiDefinition.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Drawing;
-using MonoTouch.ObjCRuntime;
-using MonoTouch.Foundation;
-using MonoTouch.UIKit;
+using ObjCRuntime;
+using Foundation;
+using UIKit;
 using AdBuddiz.Xamarin;
 
 namespace AdBuddiz.Xamarin.Internal
